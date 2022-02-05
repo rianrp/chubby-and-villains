91cb2eccce9d3383372793063cb1676ffb5dd453
rianrp <67884987+rianrp@users.noreply.github.com>
2022-02-05 00:53:33 -0300

    Update scene35.js

--- scene35.js
+++ scene35.js
@@ -1,17 +1,17 @@
-let personagem = "Personagens/teste11.png";
+let personagem = "teste11.png";
 
 function monstrinho(){
-personagem = "Personagens/teste11.png";
+personagem = "teste11.png";
 localStorage.setItem('escolha', personagem);
 alert("Escolhido com sucesso", personagem)
 }
 function yete(){
-personagem = "Personagens/teste3.png";
+personagem = "teste3.png";
 localStorage.setItem('escolha', personagem);
 alert("Escolhido com sucesso", personagem)
 }
 function douradinho(){
-personagem = "Personagens/teste6.png";
+personagem = "teste6.png";
 localStorage.setItem('escolha', personagem);
 alert("Escolhido com sucesso", personagem)
 }
